cc9846a54a22a80c6d87cc335bca09ed71d895a9
Ilan Brachmans <ilan.brachmans@gmail.com>
2021-07-26 18:03:37 -0300

    Added migrations, entities, repositories, services and controller

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,14 @@
+import "reflect-metadata";
 import express from 'express';
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-/*
-*GET    => Buscar uma informação
-*POST   => Inserir (Criar) uma informação
-*PUT    => Alterar uma informação
-*DELETE => Remover um dado
-*PATCH  => Alterar uma informação específica
-*/
-app.get("/test", (request, response) => {
-  // Request => Tudo que está entrando
-  // Response => Tudo que está saindo
-  return response.send("Olá NLW")
-});
+app.use(express.json());
 
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST")
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
